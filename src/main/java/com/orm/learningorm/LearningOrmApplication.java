2bfe62f5f0b5d2b369330acbe3288a4adce96386
Raunak Verma <72540338+Raunak0522@users.noreply.github.com>
2024-09-13 20:08:04 +0530

    first code

---
+++ src/main/java/com/orm/learningorm/LearningOrmApplication.java
@@ -0,0 +1,18 @@
+package com.orm.learningorm;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningOrmApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningOrmApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("working from project ");
+    }
+}
---
+++ src/test/java/com/orm/learningorm/LearningOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orm.learningorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
